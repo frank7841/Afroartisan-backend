9ae7a0ff2e00bec58430a8dbdad5e95065c4fb61
frankesomcarem <wakasiakafrankline@gmail.com>
2025-05-22 16:22:22 +0300

    created auth schemas for admin and user

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+export type UserSchema = User & Document;
+@Schema({timestamps:true})
+export class User{
+    @Prop({required:true})
+    name:string;
+    @Prop({required:true, unique:true})
+    email:string;
+    @Prop()
+    avatart?:string;
+    @Prop({required:true})
+    provider:string;
+    @Prop({required:true})
+    providerId:string;
+    @Prop({default:'user'})
+    role:string;
+    @Prop({default:true})
+    isActive:boolean;
+}
+export const UserSchema = SchemaFactory.createForClass(User)
